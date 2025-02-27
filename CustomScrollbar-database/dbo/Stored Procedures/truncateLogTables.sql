17a72664768852dc025f8631e2397ad8bd9881f8
jonmayor <jon_benton_mayor@outlook.com>
2025-02-27 06:11:15 -0600

    added procedure to truncate log tables

---
+++ CustomScrollbar-database/dbo/Stored Procedures/truncateLogTables.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE dbo.truncateLogTables
+as
+    TRUNCATE TABLE [dbo].[Log1];
+	TRUNCATE TABLE [dbo].[Log2];
+    TRUNCATE TABLE [dbo].[LogTreeSegmentTable];
+    TRUNCATE TABLE [dbo].[QueryRequests];
+    TRUNCATE TABLE [dbo].[logsql];
